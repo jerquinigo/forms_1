94de567a378e05bb781e07dcad635baf79d701b9
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-01-09 22:06:58 -0500

    thirdUpdate

--- forms1app/src/form.js
+++ forms1app/src/form.js
@@ -16,12 +16,16 @@ class Marsform extends Component {
       thankYouMessage: "",
       submit1: false,
       submit2: false,
-      submit3: false
+      step: 1,
+      underWater: "",
+      marital: "",
+      claustrophobic: ""
 
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.confirmedSubmit = this.confirmedSubmit.bind(this);
+    this.resetForm = this.resetForm.bind(this);
   }
 
   handleChange(event) {
@@ -60,7 +64,8 @@ handleSubmit(event){
   event.preventDefault();
   this.setState({
     message: "form completed",
-    submit1: true,
+    step: 2,
+    submit1: true
 
 
   })
@@ -71,8 +76,13 @@ confirmedSubmit(){
     message: "thank you for your application",
     submit1: false,
     submit2: true,
-    submit3: true
+    step: 3
+  })
+}
 
+resetForm(){
+  this.setState({
+    step: 1
   })
 }
 
@@ -89,39 +99,42 @@ return newArr
 
   render() {
     console.log(this.state);
-    let {name, month, day, year, country, diet, marsReason, message, thankYouMessage} = this.state
+    let {name, month, day, year, country, diet, marsReason, message, thankYouMessage, step} = this.state
     // let {diet} = this.state;
     let years = this.displayYears();
     let days = this.displayDays();
     let months = this.displayMonths();
     let theCountry = this.originCountry();
 
-if(!!this.state.submit1 === false){
+if(this.state.step === 1){
     return (
       <>
 
         <h1>Mission To Mars Registration Form</h1>
         <div>
 
-          <form onSubmit={this.handleSubmit}>
+      <form onChange={this.handleChange} onSubmit={this.handleSubmit}>
+
             <label htmlFor="name">Name</label>
             <input onChange={this.handleChange} type="text" name="name" value={this.state.name} id="name" />
             <br />
+
             <label> pick your date </label>
             <select name="year" value={this.year}onChange={this.handleChange}>
-
-              <option selected disabled value="">Year</option>
-              {years}
+              <option selected disabled value="">Year</option>{years}
             </select>
 
-            <select name="day" onChange={this.handleChange}>{days}</select>
+            <select name="day" onChange={this.handleChange}>{days}
+            </select>
 
             <select name="month" onChange={this.handleChange}>{months}</select>
 
             <br />
             <label htmlFor="country"> What is your country of origin </label>
-            <select name="country" onChange={this.handleChange}>{theCountry}</select>
+            <select name="country" onChange={this.handleChange}>{theCountry}
+            </select>
             <br />
+
           <label htmlFor="diet"> What is your dietary preference </label>
               <select name="diet" onChange={this.handleChange}>
                 <option>omnivore</option>
@@ -132,7 +145,51 @@ if(!!this.state.submit1 === false){
             <label htmlFor="why">What is your reason to be A mars explorer </label>
                 <input onChange={this.handleChange} type="text" name="why" value={this.marsReason}></input>
 
+                <p>Can you hold your Breath Underwater for 1 min?</p>
+                      <input type="radio" name="underWater" value="yes"/>
+                        <label htmlFor="underWater">Yes</label>
+                        <br />
+                      <input type="radio" name="underWater" value="no" />
+                        <label htmlFor="underWater">No</label>
+                        <br />
+                      <input type="radio" name="underWater" value="i don't know" />
+                        <label htmlFor="underWater">I dont know</label>
+                <br />
+
+                <p>What is your marital status?</p>
+                  <input type="radio" name="marital" value="Married"/>
+                    <label htmlFor="marital">Married</label>
+                    <br />
+                  <input type="radio" name="marital" value="Not Married"/>
+                    <label htmlFor="martial">Not Married</label>
+                <br />
+
+                <p>When you are in a stressful or difficult situation, how do you most frequently react?</p>
+                  <input type="radio" name="stressful" value="Determination" />
+                    <label htmlFor="stressful">Determination: I continue to confront the situation.</label>
+                  <br />
+                  <input type="radio" name="stressful" value="Defeat" />
+                    <label htmlFor="stressful">Defeat: I stop confronting the situation.</label>
+                  <br />
+                  <input type="radio" name="stressful" value="Anger" />
+                    <label htmlFor="stressful">Anger: I become upset at the situation.</label>
+                  <br />
+                  <input type="radio" name="stressful" value="Resourcefulness"/>
+                    <label htmlFor="stressful">Resourcefulness: I seek help to confront the situation.</label>
+                <br />
+
+              <p>Are you claustrophobic?</p>
+              <input type="radio" name="claustrophobic" value="yes" />
+                <label htmlFor="claustrophobic">Yes</label>
+                <br />
+              <input type="radio" name="claustrophobic" value="no" />
+                <label htmlFor="claustrophobic">No</label>
+                <br />
+              <input type="radio" name="claustrophobic" value="i don't know" />
+                <label htmlFor="claustrophobic">I don't Know </label>
+                <br />
                 <input  type="submit" checked={"this is finished"}value="SUBMIT" />
+
 
           </form>
 
@@ -144,7 +201,7 @@ if(!!this.state.submit1 === false){
 
       </>
     )
-  }else if(!!this.state.submit1 === true){
+  }else if(this.state.step === 2){
     return(
       <>
     <p>your name is: {name}</p>
@@ -159,10 +216,11 @@ if(!!this.state.submit1 === false){
     </>
     )
   }
-  else if(this.state.submit2){
+  else if(this.state.step === 3){
     return(
       <>
       <h2>{this.state.message}</h2>
+      <button onClick={this.resetForm}> Click to reset </button>
       </>
     )
   }
